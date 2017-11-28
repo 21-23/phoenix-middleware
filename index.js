89e8f7adb2fd724df57b86b13c3e5fe5674675cf
shastel <max.shastik@yandex.ru>
2017-11-28 14:02:57 +0300

    Create phoenix instance only once

--- index.js
+++ index.js
@@ -6,20 +6,25 @@ export const MESSAGE = '@@phoenix/MESSAGE';
 export const SEND = '@@phoenix/SEND';
 
 export default function createPhoenixMiddleware(options, client = WebSocket) {
+
+    let phoenix = null;
+
     return function ({ dispatch }) {
 
-        const phoenix = createPhoenix(client, options);
+        if (!phoenix) {
+            phoenix = createPhoenix(client, options);
 
-        phoenix
-            .on('connected', () => {
-                dispatch({ type: CONNECTED });
-            })
-            .on('disconnected', () => {
-                dispatch({ type: DISCONNECTED });
-            })
-            .on('message', (message) => {
-                dispatch({ type: MESSAGE, payload: message });
-            });
+            phoenix
+                .on('connected', () => {
+                    dispatch({ type: CONNECTED });
+                })
+                .on('disconnected', () => {
+                    dispatch({ type: DISCONNECTED });
+                })
+                .on('message', (message) => {
+                    dispatch({ type: MESSAGE, payload: message });
+                });
+        }
 
         return function (next) {
             return function (action) {
